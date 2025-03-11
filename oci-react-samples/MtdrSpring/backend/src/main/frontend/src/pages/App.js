2e5246493b4aa38a62421e01d0393c385b4308d0
valeaispuro <A01642923@tec.mx>
2025-03-11 14:41:29 -0600

    To do item component and User Task Visualization page

--- oci-react-samples/MtdrSpring/backend/src/main/frontend/src/pages/App.js
+++ oci-react-samples/MtdrSpring/backend/src/main/frontend/src/pages/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
+import UserTaskVisualization from "./UserTaskVisualization";
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<h2>Welcome to Home Page</h2>} />
+        <Route path="/usertasks" element= {<UserTaskVisualization/>} />
       </Routes>
     </Router>
   );
