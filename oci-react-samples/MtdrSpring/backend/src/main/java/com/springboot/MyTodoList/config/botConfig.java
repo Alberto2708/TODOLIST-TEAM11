3750a325a20220445e7ab9503b453986d87746e8
Alberto2708 <mentadoreyescarlosalberto@gmail.com>
2025-04-01 18:04:14 -0600

    added botConfig

---
+++ oci-react-samples/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/botConfig.java
@@ -0,0 +1,20 @@
+package com.springboot.MyTodoList.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import com.springboot.MyTodoList.controller.ToDoItemBotController;
+
+@Configuration
+public class botConfig {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(ToDoItemBotController bot) throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(bot);
+        return botsApi;
+    }
+}
--- oci-react-samples/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotCommands.java
+++ oci-react-samples/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotCommands.java
@@ -5,6 +5,7 @@ public enum BotCommands {
 	START_COMMAND("/start"), 
 	HIDE_COMMAND("/hide"), 
 	TODO_LIST("/todolist"),
+	FUNNY_MESSAGE("/help"),
 	ADD_ITEM("/additem");
 
 	private String command;
