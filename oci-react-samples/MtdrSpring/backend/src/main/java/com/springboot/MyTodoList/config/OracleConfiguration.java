ac8a8a4351e88ca67b2b11dc8c4feecc9d1f15a2
Pantai01 <a01276114@tec.mx>
2025-06-09 13:01:29 -0600

    Update: Modified OracleConfiguration.java testing. Trying to change DB config depending if deployment is local or not.

--- oci-react-samples/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/OracleConfiguration.java
+++ oci-react-samples/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/config/OracleConfiguration.java
@@ -32,23 +32,38 @@ public class OracleConfiguration {
     public DataSource dataSource() throws SQLException{
         OracleDataSource ds = new OracleDataSource();
 
-//        For Production       
-        ds.setDriverType(env.getProperty("driver_class_name"));
-        logger.info("Using Driver " + env.getProperty("driver_class_name"));
-        ds.setURL(env.getProperty("db_url"));
-        logger.info("Using URL: " + env.getProperty("db_url"));
-        ds.setUser(env.getProperty("db_user"));
-        logger.info("Using Username " + env.getProperty("db_user"));
-        ds.setPassword(env.getProperty("dbpassword"));
 
-//        For local testing
-//        ds.setDriverType(dbSettings.getDriver_class_name());
-//        logger.info("Using Driver " + dbSettings.getDriver_class_name());
-//        ds.setURL(dbSettings.getUrl());
-//        logger.info("Using URL: " + dbSettings.getUrl());
-//        ds.setUser(dbSettings.getUsername());
-//        logger.info("Using Username: " + dbSettings.getUsername());
-//        ds.setPassword(dbSettings.getPassword());
+
+        
+        ///// CHANGE THIS //////
+        /// Add an if for local testing vs production
+        /// 
+        
+        final boolean isLocal = false; // Set this to true for local testing
+
+
+        if (isLocal) {
+            // For local testing
+            ds.setDriverType(dbSettings.getDriver_class_name());
+            logger.info("Using Driver " + dbSettings.getDriver_class_name());
+            ds.setURL(dbSettings.getUrl());
+            logger.info("Using URL: " + dbSettings.getUrl());
+            ds.setUser(dbSettings.getUsername());
+            logger.info("Using Username: " + dbSettings.getUsername());
+            ds.setPassword(dbSettings.getPassword());
+        
+        } else {
+
+            //For Production       
+            ds.setDriverType(env.getProperty("driver_class_name"));
+            logger.info("Using Driver " + env.getProperty("driver_class_name"));
+            ds.setURL(env.getProperty("db_url"));
+            logger.info("Using URL: " + env.getProperty("db_url"));
+            ds.setUser(env.getProperty("db_user"));
+            logger.info("Using Username " + env.getProperty("db_user"));
+            ds.setPassword(env.getProperty("dbpassword"));
+        }
+        
         return ds;
     }
 }
