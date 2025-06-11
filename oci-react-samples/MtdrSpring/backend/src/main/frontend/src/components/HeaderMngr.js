e040037e14693d3b5a854d4d6c3f9179f3e1235f
Karen Corona Espinoza <110923327+KarenCorona@users.noreply.github.com>
2025-06-10 19:58:26 -0600

    Update HeaderMngr.js

--- oci-react-samples/MtdrSpring/backend/src/main/frontend/src/components/HeaderMngr.js
+++ oci-react-samples/MtdrSpring/backend/src/main/frontend/src/components/HeaderMngr.js
@@ -9,7 +9,8 @@ function HeaderMngr({
   selectedDeveloper = "all", 
   setSelectedDeveloper = () => {},
   showFilter = true,
-  onCreateTask
+  onCreateTask,
+  onCreateSubTask
 }) {
   const location = useLocation();
   const navigate = useNavigate();
@@ -100,6 +101,14 @@ function HeaderMngr({
           {showCreateButton && (
             <button className="addButton" onClick={onCreateTask}>
               Create Task
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+              </svg>
+            </button>
+          )}
+          {showCreateButton && (
+            <button className="addButton" onClick={onCreateSubTask}>
+              Create Subtask
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
               </svg>
